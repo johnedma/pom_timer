61027f952835b83703ed5f2328619027e08298ab
johned <johned.matt@gmail.com>
2016-09-27 21:00:43 -0400

    timer app and button

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+(function() {
+  //initialize variables
+  var startButton = $("#start");
+
+  //main functionality
+  startButton.on("click", countdown);
+
+
+  //function definitions
+  function countdown() {
+      alert("It's the final countdown!!!");
+
+  }
+}());
