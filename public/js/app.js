475cb24dfa3e6cc7c207179af69024a7d091e445
johned <johned.matt@gmail.com>
2016-09-28 20:13:28 -0400

    added more html and js for countdown

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,45 @@
 (function() {
   //initialize variables
   var startButton = $("#start");
+  var seconds = $("#seconds");
+  var minutes = $("#minutes");
 
   //main functionality
   startButton.on("click", countdown);
 
 
   //function definitions
-  function countdown() {
-      alert("It's the final countdown!!!");
+  function countdown(){
+    var secondsText = seconds.text();
+    var secondsTextAsNumber = parseInt(secondsText);
+    var minutesText = minutes.text();
+    var minutesTextAsNumber = parseInt(minutesText);
 
+
+    if(secondsTextAsNumber === 0) {
+      if(minutesTextAsNumber !== 0){
+      var decreasedMinutesAsNumberByOne = minutesTextAsNumber - 1;
+      var padMinutesTextAsNumber = pad(decreasedMinutesAsNumberByOne);
+      minutes.text(padMinutesTextAsNumber);
+    }
+
+    seconds.text("59");
+
+    } else {
+      var decreasedSecondsAsNumberByOne = secondsTextAsNumber - 1;
+      var padSecondsTextAsNumber = pad(decreasedSecondsAsNumberByOne);
+      seconds.text(padSecondsTextAsNumber);
+    }
   }
+
+    function pad(num){
+      if(num < 10){
+        // spit out number with a leading zero
+        return "0" + num;
+      } else {
+        // spit out the original number
+        return num;
+      }
+
+    }
 }());
